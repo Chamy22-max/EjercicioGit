42952e4810de95901c4d2f9573d9c1e6d2ac401f
Chamy22-max <kamyprada22@gmail.com>
2025-02-17 18:47:01 -0500

    Archivos de configuracion

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.printf("Hola mundo");
+
+        System.out.printf("Comando git");
+
+    }
+}
